317bd67dd90bba4c7aabf0129b6160b2935b76a1
Hamza Manzoor <35131572+MirzaHamzaManzoor@users.noreply.github.com>
2020-12-08 06:20:22 -0800

    Footer correction

--- src/features/Footer.jsx
+++ src/features/Footer.jsx
@@ -1,82 +1,93 @@
 import { Grid, makeStyles } from '@material-ui/core';
 import React from 'react';
-const useStyle=makeStyles((theme)=>({
-    myFooter:{
+const useStyle = makeStyles((theme) => ({
+    myFooter: {
         minHeight: 285,
-  display: 'flex',
-  alignItems: 'center'
+        display: 'flex',
+        alignItems: 'center'
     },
-    footerList:{
+    footerList: {
         listStyleType: 'none',
         margin: 0,
         paddingLeft: 0,
-        '&>li':{
+        '&>li': {
             marginTop: 5,
             marginBottom: 5,
-          
-          },
-          '&>li>a':{
+
+        },
+        '&>li>a': {
             color: 'black',
             fontSize: 18
-          }
+        }
     },
-    textRight:{
-        fontSize:18,
+    textRight: {
+        fontSize: 18,
         textAlign: 'right',
         [theme.breakpoints.down('md')]: {
             textAlign: 'center !important'
         }
     },
+    footerGridRight: {
+        [theme.breakpoints.down('md')]: {
+            paddingLeft: '0 !important'
+        }
+    },
+    footerGridLeft: {
+        [theme.breakpoints.down('md')]: {
+            paddingRight: '0 !important'
+        }
+    },
+
 }))
 function Footer(props) {
-    const classes=useStyle()
+    const classes = useStyle()
     return (
         <footer className={classes.myFooter}>
-        <Grid container spacing={3}>
-            <Grid item xs={5} md={6}>
-            <ul className={classes.footerList}>
-                <li>
-                    <a href='!#'>
-                        Email us
+            <Grid container spacing={3}>
+                <Grid item xs={5} md={6} className={classes.footerGridLeft}>
+                    <ul className={classes.footerList}>
+                        <li>
+                            <a href='!#'>
+                                Email us
                     </a>
-                </li>
-                <li>
-                    <a href='!#'>
-                        Follow us
+                        </li>
+                        <li>
+                            <a href='!#'>
+                                Follow us
                     </a>
-                </li>
-                <li>
-                    <a href='!#'>
-                    Changelog
+                        </li>
+                        <li>
+                            <a href='!#'>
+                                Changelog
                     </a>
-                </li>
-            </ul>
-            <ul  className={classes.footerList}>
-                <li>
-                    <a href='!#'>
-                        Email us
+                        </li>
+                    </ul>
+                    <ul className={classes.footerList}>
+                        <li>
+                            <a href='!#'>
+                                Email us
                     </a>
-                </li>
-                <li>
-                    <a href='!#'>
-                        Follow us
+                        </li>
+                        <li>
+                            <a href='!#'>
+                                Follow us
                     </a>
-                </li>
-                <li>
-                    <a href='!#'>
-                    Changelog
+                        </li>
+                        <li>
+                            <a href='!#'>
+                                Changelog
                     </a>
-                </li>
-            </ul>
-        
-            </Grid>
-            <Grid item xs={7} md={6}>
-         <div className={classes.textRight}>
-             &copy; 2020 Templateondemand.co
+                        </li>
+                    </ul>
+
+                </Grid>
+                <Grid item xs={7} md={6} className={classes.footerGridRight}>
+                    <div className={classes.textRight}>
+                        &copy; 2020 Templateondemand.co
          </div>
+                </Grid>
+
             </Grid>
-       
-        </Grid>
         </footer>
     );
 }
